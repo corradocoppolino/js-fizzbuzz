882d521838eb20c9fe94e0808028f65db4586361
corradocoppo <corradocoppolino@gmail.com>
2021-04-09 15:59:10 +0200

    finito anche senza vettori

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-var array100 = [];
+// prima tentativo con vettori
+
+/* var array100 = [];
 
 for(var i=1; i < 101; i++) {
     
@@ -13,11 +15,30 @@ for(var i=1; i < 101; i++) {
         array100.push(i);
     }
 
-    document.getElementById("scala").innerHTML = array100[array100.length-1];
+    document.getElementById("scala").innerHTML += array100[array100.length-1] + " ";
      
 }
 
+console.log(array100); */
 
-console.log(array100);
+// tentativo senza vettori
+
+for(var i=1; i < 101; i++) {
+    
+    if(i%5==0 && i%3==0){
+        console.log("FizzBuzz");
+        document.getElementById("scala").innerHTML += "FizzBuzz" + " ";
+    }else if(i%5==0){
+        console.log("Buzz");
+        document.getElementById("scala").innerHTML += "Buzz" + " ";
+    }else if(i%3==0){
+        console.log("Fizz");
+        document.getElementById("scala").innerHTML += "fizz" + " ";
+    }else{
+        console.log(i);
+        document.getElementById("scala").innerHTML += i + " ";
+    }
+
+}
 
 
